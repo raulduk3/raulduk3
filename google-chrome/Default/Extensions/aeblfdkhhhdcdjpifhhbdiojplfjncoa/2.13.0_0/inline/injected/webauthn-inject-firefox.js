38a21528838ca4b005ddc09ccf6d22c3ce8f7d04
Ricky Alvarez <rawalvarez731@gmail.com>
2023-08-15 15:05:35 -0400

    Dotfiles

--- google-chrome/Default/Extensions/aeblfdkhhhdcdjpifhhbdiojplfjncoa/2.13.0_0/inline/injected/webauthn-inject-firefox.js
+++
@@ -1,6 +0,0 @@
-(function () {
-	'use strict';
-
-	if(!0!==window.webauthnFirefoxContentScriptInjected){window.webauthnFirefoxContentScriptInjected=!0;const e=document.createElement("script");e.src=chrome.runtime.getURL("/inline/injected/webauthn-listeners.js"),document.documentElement.prepend(e),e.remove();}
-
-})();
--- google-chrome/Default/Extensions/hfpokjeoalngkknelljbenaobinichjf/2.1.1_0/background.js
+++
@@ -1,1 +0,0 @@
-chrome.action.onClicked.addListener((e=>{chrome.scripting.insertCSS({target:{tabId:e.id},files:["guides.css"]}),chrome.scripting.executeScript({target:{tabId:e.id},files:["guides.js"]})}));
