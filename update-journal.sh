7408f2dc07e002372b3507c3d69fabbe45282c33
ra - our lady <rawalvarez731@gmail.com>
2024-07-25 12:18:09 -0500

    new stuff

---
+++ update-journal.sh
@@ -0,0 +1,11 @@
+# Description: Update the journal repository
+# Author: Ricky Alvarez
+# Last updated: 2024-07-19
+
+cd ~/Journal/
+git pull --force
+git add -A
+git commit -a -m "updated journal"
+git push
+
+
---
+++ update-resume.sh
@@ -0,0 +1,6 @@
+cp ~/Resume:CVs/richardalvarez-cs-cv/richardalvarez.pdf ~/Dev/raulduke/public/Richard_Alvarez_Resume.pdf
+cp ~/Resume:CVs/richardalvarez-cs-cv/richardalvarez.pdf Journal/ra-general/3.\ general\ notes/CS/Resume.pdf
+cd ~/Dev/raulduke/
+git add -A
+git commit -a -m "new resume"
+git push
