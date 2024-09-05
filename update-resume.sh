de89ffaf1f0d956ff5860bdfc5ab20f32ce65ac0
Richard Alvarez (wind-child) <rawalvarez731@gmail.com>
2024-09-05 13:51:31 -0500

    ay

--- update-resume.sh
+++ update-resume.sh
@@ -1,5 +1,4 @@
 cp ~/Resume:CVs/richardalvarez-cs-cv/richardalvarez.pdf ~/Dev/raulduke/public/Richard_Alvarez_Resume.pdf
-cp ~/Resume:CVs/richardalvarez-cs-cv/richardalvarez.pdf Journal/ra-general/3.\ general\ notes/CS/Resume.pdf
 cd ~/Dev/raulduke/
 git add -A
 git commit -a -m "new resume"
