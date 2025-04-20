c34f165eb912baece629a8a5baf08ac346095ea8
ra - our lady <rawalvarez731@gmail.com>
2025-04-20 11:58:04 -0500

    yay

--- update-resume.sh
+++ update-resume.sh
@@ -1,4 +1,5 @@
-cp ~/Resume:CVs/richardalvarez-cs-cv/richardalvarez.pdf ~/Dev/raulduke/public/Richard_Alvarez_Resume.pdf
+cp ~/Resume:CVs/richardalvarez-cs-cv/Richard_Alvarez_Resume.pdf ~/Dev/raulduke/public/Richard_Alvarez_Resume.pdf
+cp ~/Resume:CVs/richardalvarez-cs-cv/Richard_Alvarez_Resume.pdf ~/Journal/ra-general/2.\ general\ notes/CS\ and\ IT/Resume.pdf
 cd ~/Dev/raulduke/
 git add -A
 git commit -a -m "new resume"
